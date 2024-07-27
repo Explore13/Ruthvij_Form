367f420682835d64f71d3c028cce5628851760e7
Surya Ghosh <suryaghoshchemistry2k21@gmail.com>
2024-07-27 14:44:12 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,34 +1,30 @@
 import express from 'express';
-import helmet from 'helmet';
 import cors from 'cors';
-import router from './routes/sendEmail.routes.js'; // Ensure the correct path
+import helmet from 'helmet';
+import router from './routes/sendEmail.routes.js';
 
 const app = express();
 
-// Use Helmet to enhance security
+// Use Helmet for security
 app.use(helmet());
 
 // Configure CORS
 app.use(cors({
-  origin: 'https://ruthvij-form.vercel.app', // Allow requests from this origin
+  origin: 'https://ruthvij-form.vercel.app', // Allow this origin
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 }));
 
-// Middleware to parse JSON
 app.use(express.json());
-
-// Serve static files from the 'public' directory
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Define routes
+app.use('/api', router);
+
 app.get('/', (req, res) => {
   res.send('Hello from Server');
 });
 
-app.use('/api', router); // Ensure /api routes are correctly defined
-
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
